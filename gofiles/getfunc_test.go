23498e461714bdfa44f2d4febac89dd0191a379b
Johan Aanesen <johanaan@stud.ntnu.no>
2017-11-09 11:56:58 +0100

    derp

--- gofiles/getfunc_test.go
+++ gofiles/getfunc_test.go
@@ -32,30 +32,18 @@ func TestAdd2d(t *testing.T) {
 func TestGetValue(t *testing.T) {
 	out := []string{"NOK", "EUR"}
 
-	// Get today's date in date format
-	tempToday := time.Now().Local()
-	today := tempToday.Format("2006-01-02")
-
 	// Set up the database
 	db := SetupDB()
 	db.Init()
 
 	// Get today's currencies for today
-	data2d, ok := db.GetLatest(today)
+	data2d, ok := db.GetLatest("noDate")
 
-	// If there isn't any data in the db for today
+	// If there isn't any data in the db
 	if ok == false {
-
-		// Try to get data from yesterday
-		tempToday = time.Now().Local().AddDate(0, 0, -1)
-		yesterday := tempToday.Format("2006-01-02")
-		data2d, ok = db.GetLatest(yesterday)
-
-		// If there's still not any data: log error to heroku
-		if ok == false {
-			log.Println("Could not get any data from today or yesterday:/", 404)
-		}
+		log.Println("Could not get any data:/", 404)
 	}
+
 	realValue := data2d.Data[out[0]][out[1]]
 
 	testValue := GetValue(out[0], out[1])
